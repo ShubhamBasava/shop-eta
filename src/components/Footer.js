c7e738e94320d9986db992f587fd324c49362d35
ShubhamBasava <srbasava09@gmail.com>
2023-10-29 19:07:12 +0530

    e enter the commit message for your changes. Lines starting

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react';
 
 const Footer = () => {
   return (
-    <footer
-      className="page-footer font-small text-light border-top"
-      style={{ backgroundColor: "rgba(0,0,0,.9)" }}
-    >
+    <footer className="page-footer font-small text-light border-top" style={{ backgroundColor: 'rgba(0,0,0,.9)' }}>
       <div className="footer-copyright text-center py-3">
         © {new Date().getFullYear()} Copyright
-        <a href="#"> ShopVita.com</a>
+        <button
+          className="btn btn-link text-light"
+          onClick={() => {
+            // Handle the button click event, e.g., navigate to a different page or perform an action.
+          }}
+        >
+          ShopVita.com
+        </button>
       </div>
     </footer>
   );
