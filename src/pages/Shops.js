3fd3e887edef1f12fb04ecdd70e35cdc44168379
ShubhamBasava <srbasava09@gmail.com>
2023-10-30 08:49:59 +0530

    feat: Create Shops component to display shop information
    - Added a new functional component, Shops, to display shop information fetched from a database.
    - Fetched shop data from the backend API.
    - Rendered shop details, including shop name, category, customers, shop status, and lease status.
    
    This commit enhances the application by providing a Shops page for users to explore various shops. The shop data is retrieved dynamically from the database.

--- src/pages/Shops.js
+++ src/pages/Shops.js
@@ -1,39 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 
 const Shops = () => {
+  const [shops, setShops] = useState([]);
+
+  // Fetch shop data from your API or database
+  useEffect(() => {
+    // You can make an API call here to fetch shop data and set it in the state.
+    // Example fetch call:
+    fetch('/api/shops')
+      .then((response) => response.json())
+      .then((data) => setShops(data))
+      .catch((error) => console.error('Error fetching shops', error));
+  }, []);
+
   return (
     <div>
       <div className="container-xxl shop-bg mb-3">
-        <div
-          className="row justify-content-center align-items-center text-white"
-          style={{ height: "100vh", verticalAlign: "middle" }}
-        >
+        <div className="row justify-content-center align-items-center text-white" style={{ height: "100vh", verticalAlign: "middle" }}>
           <div className="col-md-6 text-center d-block">
             <h1 className="display-1 text-nowrap">Shops</h1>
-            <p className="lead fst-italic">
-              Welcome to the Shops page. Here, you can explore various shops.
-            </p>
+            <p className="lead fst-italic">Welcome to the Shops page. Here, you can explore various shops.</p>
           </div>
         </div>
       </div>
 
-      <div className="row justify-content-center mt-5">
-        <div className="col-md-1 text-center d-block"></div>
-        <div className="col-md-4 text-center text-white">
-          <h2>Shop List</h2>
-          <p className="lead fst-italic mt-5 text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-            nisi autem neque, corrupti velit obcaecati nam! Nulla delectus,
-            quaerat, voluptatum obcaecati eum quam, fugiat aliquid aspernatur
-            consectetur accusantium illum non.
-          </p>
-          <p className="lead fst-italic text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita
-            nisi autem neque, corrupti velit obcaecati nam! Nulla delectus,
-            quaerat, voluptatum obcaecati eum quam, fugiat aliquid aspernatur
-            consectetur accusantium illum non.
-          </p>
-        </div>
+      <div className="container-xxl py-5 ">
+        {shops.map((shop) => (
+          <div className="row justify-content-around m-0 text-white" key={shop.shopId}>
+            <div className="col-md-4">
+              <div className="card shadow px-4 py-5 text-dark">
+                <h2 className="fw-bold">{shop.shopName}</h2>
+                <p className="lead fs-6 fst-italic">
+                  Category: {shop.shopCategory}<br />
+                  Customers: {shop.customers}<br />
+                  Shop Status: {shop.shopStatus}<br />
+                  Lease Status: {shop.leaseStatus}
+                </p>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
