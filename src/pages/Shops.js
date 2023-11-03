cf35d9243acfd56b2df996c750b0754622cc3a37
ShubhamBasava <srbasava09@gmail.com>
2023-11-03 20:27:55 +0530

    git commit -m "feat: Enhanced Shops.js functionality
    - Set 'GET' as the default HTTP method for data retrieval.
    - Implemented a dropdown to select different HTTP methods.
    - Updated data display to show shop information in a table.
    - Modified the API URL to use Spring Tools (http://localhost:8080/shops).
    "

--- src/pages/Shops.js
+++ src/pages/Shops.js
@@ -3,7 +3,8 @@ import React, { useState, useEffect } from "react";
 const Shops = () => {
   const [shops, setShops] = useState([]);
   const [shopId, setShopId] = useState("");
-  const apiUrl = "http://localhost:8080/shops"; // Replace with your API endpoint
+  const [httpMethod, setHttpMethod] = useState("GET"); // Set "GET" as the default method
+  const apiUrl = "http://localhost:8080/shops";
 
   useEffect(() => {
     // Fetch all shop data from the API
@@ -15,13 +16,13 @@ const Shops = () => {
 
   const handleShopIdSubmit = (event) => {
     event.preventDefault();
-    
-    // Fetch data for a specific shop using shopId
-    const shopApiUrl = `${apiUrl}/${shopId}`;
-    
+
+    // Fetch data for a specific shop using shopId and httpMethod
+    const shopApiUrl = `${apiUrl}/${shopId}?method=${httpMethod}`;
+
     fetch(shopApiUrl)
       .then((response) => response.json())
-      .then((data) => setShops([data])) // Display only the selected shop
+      .then((data) => setShops([data]))
       .catch((error) => console.error("Error fetching shop data:", error));
   };
 
@@ -40,16 +41,16 @@ const Shops = () => {
           </div>
         </div>
       </div>
-      <div className="container-xxl py-5 ">
+      <div className="container-xxl py-5">
         <div className="row justify-content-around m-0 text-white">
           <div className="col-md-4">
             <div className="card shadow px-4 py-5 text-dark">
               <form onSubmit={handleShopIdSubmit}>
                 <div className="row">
                   <div className="col-md-12">
-                    <h2 className="fw-bold">To View Shops Details</h2>
-                    <p className="lead fs-7 fst-italic  ">
-                      Enter The <strong>Shop ID </strong>{" "}
+                    <h2 className="fw-bold">View Shops Details</h2>
+                    <p className="lead fs-7 fst-italic">
+                      Enter the <strong>Shop ID</strong> and select an HTTP method.
                     </p>
                   </div>
                   <div className="divider div-transparent mt-3 mb-5"></div>
@@ -67,6 +68,25 @@ const Shops = () => {
                     </div>
                   </div>
                   <div className="col-md-12">
+                    <div className="form-floating mb-3">
+                      <select
+                        className="form-select"
+                        id="httpMethod"
+                        name="HttpMethod"
+                        value={httpMethod}
+                        onChange={(e) => setHttpMethod(e.target.value)}
+                      >
+                        <option value="GET">GET</option>
+                        <option value="POST">POST</option>
+                        <option value="PUT">PUT</option>
+                        <option value="DELETE">DELETE</option>
+                      </select>
+                      <label htmlFor="httpMethod" className="text-dark">
+                        &nbsp;Select HTTP Method
+                      </label>
+                    </div>
+                  </div>
+                  <div className="col-md-12">
                     <div className="form-group">
                       <input
                         type="submit"
@@ -79,25 +99,32 @@ const Shops = () => {
               </form>
             </div>
           </div>
-          {shops.map((shop) => (
-            <div className="col-md-4" key={shop.shopId}>
-              <div className="card shadow px-4 py-5 text-dark">
-                <h2 className="fw-bold">{shop.shopName}</h2>
-                <p className="lead fs-6 fst-italic">
-                  Category: {shop.shopCategory}
-                </p>
-                <p className="lead fs-6 fst-italic">
-                  Customers: {shop.customers}
-                </p>
-                <p className="lead fs-6 fst-italic">
-                  Shop Status: {shop.shopStatus}
-                </p>
-                <p className="lead fs-6 fst-italic">
-                  Lease Status: {shop.leaseStatus}
-                </p>
-              </div>
-            </div>
-          ))}
+          <div className="col-md-8">
+            <table className="table table-bordered">
+              <thead>
+                <tr>
+                  <th>Shop ID</th>
+                  <th>Shop Name</th>
+                  <th>Category</th>
+                  <th>Customers</th>
+                  <th>Shop Status</th>
+                  <th>Lease Status</th>
+                </tr>
+              </thead>
+              <tbody>
+                {shops.map((shop) => (
+                  <tr key={shop.shopId}>
+                    <td>{shop.shopId}</td>
+                    <td>{shop.shopName}</td>
+                    <td>{shop.shopCategory}</td>
+                    <td>{shop.customers}</td>
+                    <td>{shop.shopStatus}</td>
+                    <td>{shop.leaseStatus}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </div>
